fed728b598d4391370188c565c67fe1c36fe6cf3
KseniyaChepelevich <chepelevich.qamid.5@gmail.com>
2021-12-14 09:40:48 +0300

    added test

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,70 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldReturn0IfAmountOn1000() {
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+
+
+    }
+
+    @Test
+    public void shouldReturn1IfAmountOn999() {
+        int amount = 999;
+
+        int actual = service.remain(amount);
+        int expected = 1;
+
+        assertEquals(expected, actual);
+
+
+    }
+
+    @Test
+    public void shouldReturn999IfAmountOn1001() {
+        int amount = 1001;
+
+        int actual = service.remain(amount);
+        int expected = 999;
+
+        assertEquals(expected, actual);
+
+
+    }
+
+    @Test
+    public void shouldReturn0IfAmountOn2000() {
+        int amount = 2000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+
+
+    }
+
+    @Test
+    public void shouldReturn999IfAmountOn1() {
+        int amount = 1;
+
+        int actual = service.remain(amount);
+        int expected = 999;
+
+        assertEquals(expected, actual);
+
+
+    }
+
+}
